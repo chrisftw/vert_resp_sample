bdaf647b22efaa07e11f3ac72f08111dc913085c
Chris Reister <chris@greenlightapps.com>
2011-12-20 14:15:41 -0800

    allows unknown number of inputs in the add function

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -15,6 +15,12 @@ describe StringCalc do
       StringCalc.add("5,2").should == 7
     end
     
+    it "should accept a string with unknown number of values (comma seprated)" do
+      StringCalc.add("").should == 0
+      StringCalc.add("2,3,5,7").should == 17
+      StringCalc.add("11,13,17,19,23").should == 83
+    end
+    
   end
   
 end
