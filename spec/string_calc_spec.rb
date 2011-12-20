8806b743e1ac9dd17c55dcd50f5bb028d7e04b26
Chris Reister <chris@greenlightapps.com>
2011-12-20 14:41:59 -0800

    Raise error on double delimiter in add method

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -29,6 +29,13 @@ describe StringCalc do
       StringCalc.add("1\n2,3").should == 6
     end
     
+    it "should not allow 2 delimeters at the end of the string" do
+      # I am unsure why we can not allow two delimiters at the end of a input string. Seems more stable to allow it,
+      # and will only add 0's to the sum.  But will make it not work as specified.
+      StringCalc.add("1,\n").should raise_error
+    end
+    
+    
   end
   
 end
--- string_calc.rb
+++ string_calc.rb
@@ -4,7 +4,10 @@ class StringCalc
   
   # read in a string, parse the string and add the numbers.
   def self.add(values)
-    int_values = values.split(/,|\n/).collect{|num| num.to_i}
+    if(values.index(/(,|\n){2}/)) # not sure why we are checking for this, but it is in the spec to not allow it.
+      raise RuntimeError, "Double Delimiter Foul"
+    end
+    int_values = values.split(/,|\n/).collect{ |num| num.to_i }
     return 0 if int_values.empty?
     return int_values.inject(:+)
   end
